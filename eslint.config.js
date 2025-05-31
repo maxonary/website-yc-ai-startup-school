078c1b343ed9ee8401b637110c3deff805be826c
Maximilian Arnold <maximilian.arnold1@porsche.de>
2025-06-01 01:56:45 +0200

    feat: ESLint configuration for HTML files

--- eslint.config.js
+++ eslint.config.js
@@ -2,21 +2,27 @@ import eslintPluginTs from '@typescript-eslint/eslint-plugin';
 import parserTs from '@typescript-eslint/parser';
 
 export default [
-  {
-    files: ['**/*.ts'],
-    languageOptions: {
-      parser: parserTs,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
+    {
+        files: ['**/*.ts'],
+        languageOptions: {
+        parser: parserTs,
+        parserOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+        },
+        },
+        plugins: {
+        '@typescript-eslint': eslintPluginTs,
+        },
+        rules: {
+        semi: ['error', 'always'],
+        quotes: ['error', 'single'],
+        },
     },
-    plugins: {
-      '@typescript-eslint': eslintPluginTs,
-    },
-    rules: {
-      semi: ['error', 'always'],
-      quotes: ['error', 'single'],
-    },
-  },
+    {
+    files: ['**/*.html'],
+    languageOptions: {},
+    plugins: {},
+    rules: {},
+    }
 ];
