f59c8cc3529aca4516d2d6805a1562ef62c367ea
Maximilian Arnold <maximilian.arnold1@porsche.de>
2025-06-01 02:01:23 +0200

    style: format eslint.config.js for consistency

--- eslint.config.js
+++ eslint.config.js
@@ -2,27 +2,27 @@ import eslintPluginTs from '@typescript-eslint/eslint-plugin';
 import parserTs from '@typescript-eslint/parser';
 
 export default [
-    {
-        files: ['**/*.ts'],
-        languageOptions: {
-        parser: parserTs,
-        parserOptions: {
-            ecmaVersion: 'latest',
-            sourceType: 'module',
-        },
-        },
-        plugins: {
-        '@typescript-eslint': eslintPluginTs,
-        },
-        rules: {
-        semi: ['error', 'always'],
-        quotes: ['error', 'single'],
-        },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: parserTs,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
     },
-    {
+    plugins: {
+      '@typescript-eslint': eslintPluginTs,
+    },
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+    },
+  },
+  {
     files: ['**/*.html'],
     languageOptions: {},
     plugins: {},
     rules: {},
-    }
+  },
 ];
